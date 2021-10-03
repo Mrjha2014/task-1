9ddab5056e3dbea85ac707fa9eb9d31400475206
Mrjha2014 <bds.ashu@gmail.com>
2021-10-03 13:42:24 +0530

    1

--- src/App.js
+++ src/App.js
@@ -5,9 +5,6 @@ import Footer from "./Footer";
 import Card from "./Card";
 import Slider from "./Slider";
 import Contant from "./Contant";
-import About from "./About";
-import Contact from "./Contact";
-import { Route, Link } from "react-router-dom";
 
 export default function App() {
   return (
